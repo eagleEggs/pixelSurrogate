1860c492543e1cc6545defcc8ea482800630ad57
nachoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo <eric.mitchell.r@gmail.com>
2018-02-18 02:04:41 -0500

    Added editor, features, and inspector details

--- scripts/pixelSurrogateController.cs
+++ scripts/pixelSurrogateController.cs
@@ -1,52 +1,69 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using System.IO;
 using System.Linq;
-using UnityEngine.SceneManagement;
 
 public class pixelSurrogateController : MonoBehaviour
 {
 
     [ExecuteInEditMode]
-
+    [Header("Texture Settings:")]
     [SerializeField]
-    private Texture2D sourceTex; // source texture to copy
+    private Texture2D sourceTexture; // source texture to copy
     [SerializeField] private Rect sourceRect; // rect reference that sets dimensions to read source
-    [SerializeField] private int setWidth; // width of texture
-    [SerializeField] private int setHeight; // height of teture
-    private Texture2D destTex2; // destination texture to write
+    [SerializeField] private float setWidth; // width of texture
+    [SerializeField] private float setHeight; // height of teture
+    private Texture2D destinationTexture; // destination texture to write
     private Color[] destPix; // destination texture pixel array
+    [Header("Mesh Options: Fastest is Quad")]
+    [SerializeField]
+    private bool enableColliders = false; // create with colliders?
+    [SerializeField] private bool useCubesInsteadOfQuads = false; // create with cubes instead of quads?
+    [SerializeField] private PrimitiveType meshType = PrimitiveType.Quad; // to store the selection of mesh type
 
 
     void Start()
     {
 
 
+    }
+
+    public void  updateWH(){
+
+        sourceRect.width = sourceTexture.width;
+        sourceRect.height = sourceTexture.height;
+        setWidth = sourceRect.width;
+        setHeight = sourceRect.height;
 
     }
 
-
-
     public void hatchemall()
     {
-
-
-        List<Color> colorsFinal = new List<Color>(); // color list
-
+        
         // all x and y of the texture to iterate
         int x = Mathf.FloorToInt(sourceRect.x);
         int y = Mathf.FloorToInt(sourceRect.y);
         int width = Mathf.FloorToInt(sourceRect.width);
         int height = Mathf.FloorToInt(sourceRect.height);
 
-        Color[] pix = sourceTex.GetPixels(x, y, width, height); // get the source texture pixels into the array
+        Color[] pix = sourceTexture.GetPixels(x, y, width, height); // get the source texture pixels into the array
 
-        destTex2 = new Texture2D(width, height); // set the dimensions of teh output texture, must be equal to source
+        destinationTexture = new Texture2D(width, height); // set the dimensions of teh output texture, must be equal to source
 
         int n = 0; // x iteration
         int nn = 0; // y iteration
+
+        if(!useCubesInsteadOfQuads){ // decide before processing whether quad or cube is enabled within the inspector
+
+            meshType = PrimitiveType.Quad; // set varibale to quad
+
+        }else{  // else set varibale to cube
+
+            meshType = PrimitiveType.Cube;
+
+        }
 
         foreach (Color cs in pix) // for each color in the color array from source texture
         {
@@ -55,15 +72,14 @@ public class pixelSurrogateController : MonoBehaviour
             {
                 if (n == setWidth) { n = 0; nn++; } // if x = total width, set it to 0, increase y
 
-                colorsFinal.Add(cs); // add the color from the source to to list
+                // create pixel meshes:
 
-                // create quads:
-                var go = GameObject.CreatePrimitive(PrimitiveType.Quad); // create
+                var go = GameObject.CreatePrimitive(meshType); // create as quad
                 Color mesh = go.GetComponent<Renderer>().material.color = cs; // set it's color
                 go.AddComponent<pixelSurrogate_hatch>(); // add script to each mesh for AI behaviour
                 go.transform.position = new Vector3(n, nn, transform.position.z); // put the quad in the same x/y as the texture
-                destTex2.SetPixel(n, nn, cs); // set the textures pixel to mirror the texture
-                Destroy(go.GetComponent<Collider>());
+                destinationTexture.SetPixel(n, nn, cs); // set the textures pixel to mirror the texture
+                go.GetComponent<Collider>().enabled = enableColliders; // set colliders on/of based on bool in inspector
 
                 n++; // iterate x
 
@@ -73,7 +89,7 @@ public class pixelSurrogateController : MonoBehaviour
         }
 
 
-        destTex2.Apply(); // apply the changes to the texture
+        destinationTexture.Apply(); // apply the changes to the texture
 
 
 
---
+++ scripts/pixelSurrogateEditor.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(pixelSurrogateController))]
+public class pixelSurrogateEditor : Editor
+{
+
+    private GUIStyle miniLabelStyle = new GUIStyle();
+
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+
+        pixelSurrogateController matGate = (pixelSurrogateController)target;
+        miniLabelStyle.wordWrap = true;
+        GUILayout.Space(6);
+        EditorGUILayout.LabelField("General Controls", EditorStyles.boldLabel);
+        GUILayout.Label("Texture to Pixels Settings");
+        EditorGUILayout.BeginVertical("Box");
+        GUILayout.Label("Select your Source Texture, Click Update, then Hatchemall. This will store the Width and Height of the texture for you. WARNING: Using anything other than Quads or a texture over 128x128 may freeze. This will generate a lot of objects in the scene!", miniLabelStyle);
+        EditorGUILayout.EndVertical();
+        EditorGUILayout.BeginVertical("Box");
+        if (GUILayout.Button("Update"))
+        {
+            matGate.updateWH();
+        }
+        if (GUILayout.Button("Hatchemall"))
+        {
+            matGate.hatchemall();
+        }
+        EditorGUILayout.EndVertical();
+
+
+
+
+
+    }
+
+
+
+
+}
